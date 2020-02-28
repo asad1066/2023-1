6d0f320542542837a0e698a525695283c4dac25f
William Sadler <andysadler@seattleacademy.org>
2020-02-28 08:54:16 -0800

    Add attendance server feture

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 const express = require('express')
 const app = express()
 const port = 1501
+const attendace = [];
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) => {
+	let query = req.query;
+	attendace.push(query.name);
+	console.log(attendace); 
+	res.send(query)
+})
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
